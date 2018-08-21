bd4fb2a1e6d70aa2e1472926e21a14d9163b3448
Jan Gottweiss <jan.gottweiss@yahoo.de>
2018-08-21 21:40:15 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ColorInput/>
+  <div id="container" v-bind:style="{ backgroundColor: bgColor}">   
+    <div id="app">
+      <ColorInput @newColor="newColor" v-bind:startColor="bgColor" />
+    </div>
+    
   </div>
 </template>
 
 <script>
+import chroma from "chroma-js";
 import ColorInput from './components/ColorInput.vue'
 
+const startColor = "#D4F880";
 
 export default {
   name: 'app',
   components: {
     ColorInput
+  },
+  data() {
+    return {
+      bgColor: startColor,
+      color: chroma(startColor)
+    }
+  },
+  methods: {
+    newColor(c) {
+      this.bgColor = c.hex();
+    }
   }
 }
 </script>
 
 <style>
-#app {
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+#container {
+  width: 100;
+  height: 100%;
+  position: absolute;
+  top: 0; bottom: 0px; left: 0px; right: 0px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app {
+  margin: 50px;
 }
 </style>
--- src/components/ColorInput.vue
+++ src/components/ColorInput.vue
@@ -1,35 +1,83 @@
 <template>
-  <div class="component">
-      <h3>{{ name }}</h3>
-    <input type="text">
+  <div class="component">  
+    <el-row :gutter="20" type="flex" justify="space-around">
+      <el-col :span="18"><div>
+         <el-input    
+        size="large"
+        placeholder="Please Input"
+        v-model="colorInput">
+        </el-input>  
+        <el-color-picker v-model="colorInputPicker" @change="colorPickerChange"></el-color-picker>    
+      </div>
+    </el-col>
+    </el-row>
+                 
   </div>
 </template>
 
 <script>
+import chroma from "chroma-js";
 export default {
   name: "ColorInput",
+  props: ['startColor'],
+  created: function () {    
+    this.colorObject = chroma(this.startColor);
+    this.colorInputPicker = this.colorObject.hex();
+    this.colorInput = this.colorObject.hex();
+  },
   data() {
     return {
-      name: "TEST"
+      colorInput: "#D4F880",
+      colorObject: chroma("#D4F880"),
+      colorInputPicker: "#D4F880"
     };
+  },
+  methods: {
+    newColor(c) {
+      this.$emit("newColor", c);
+    },
+    colorPickerChange() {    
+      this.colorObject = chroma(this.colorInputPicker); 
+      this.colorInput = this.colorObject.hex();
+      this.newColor(this.colorObject);
+    }
+  },
+  watch: {   
+    colorInput: function(newColorInput) {
+      let color;
+      try {
+        this.colorObject = chroma(newColorInput);        
+        this.newColor(this.colorObject);
+        this.colorInputPicker = this.colorObject.hex();
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.el-col {
+  border-radius: 4px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.bg-purple-dark {
+  background: #99a9bf;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.bg-purple {
+  background: #d3dce6;
 }
-a {
-  color: #42b983;
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
 }
 </style>
