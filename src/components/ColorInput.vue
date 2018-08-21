263c4547a7ebde22e97241674753699b5bcee232
D059273 <jan.gottweiss@sap.com>
2018-08-21 16:19:23 +0200

    initial commit

---
+++ src/components/ColorInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="component">
+      <h3>{{ name }}</h3>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorInput",
+  data() {
+    return {
+      name: "TEST"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
